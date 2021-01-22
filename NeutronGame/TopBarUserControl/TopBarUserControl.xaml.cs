5efc068eaec558b719246f1dcd69109d0ed08dc0
tartie sorel <sorelaugustin@gmail.com>
2021-01-22 12:19:34 +0000

    Top bar event added

--- NeutronGame/TopBarUserControl/TopBarUserControl.xaml.cs
+++ NeutronGame/TopBarUserControl/TopBarUserControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace NeutronGame
 {
@@ -29,6 +30,45 @@ namespace NeutronGame
         public TopBarUserControl()
         {
             InitializeComponent();
+            AddMouseUpEventToEllipse();
+            AddDragAndDropEventToRectangle();
+            AddButtonCloseHoverEvent();
+            AddButtonMinimizeHoverEvent();
         }
+
+        #region Button Minimize Hover Event
+        private void AddButtonMinimizeHoverEvent()
+        {
+            ButtonMinimize.MouseEnter += (s, e) => ButtonMinimize.Width = ButtonMinimize.Height = 13;
+            ButtonMinimize.MouseLeave += (s, e) => ButtonMinimize.Width = ButtonMinimize.Height = 10;
+        }
+        #endregion
+
+        #region Button Close Hover Event
+        private void AddButtonCloseHoverEvent()
+        {
+            ButtonClose.MouseEnter += (s, e) => ButtonClose.Width = ButtonClose.Height = 13;
+            ButtonClose.MouseLeave += (s, e) => ButtonClose.Width = ButtonClose.Height = 10;
+        }
+        #endregion
+
+        #region Add Drag And Drop Event To Rectangle
+        private void AddDragAndDropEventToRectangle()
+        {
+            RectangleDragZone.MouseDown += (s, e) =>
+            {
+                if (e.ChangedButton == System.Windows.Input.MouseButton.Left)
+                    (Application.Current.Windows[0] as MainWindow).DragMove();
+            };
+        }
+        #endregion
+
+        #region Add Mouse Up Event To Ellipse
+        private void AddMouseUpEventToEllipse()
+        {
+            ButtonClose.MouseLeftButtonUp += (s, e) => Application.Current.Shutdown();
+            ButtonMinimize.MouseLeftButtonUp += (s, e) => (Application.Current.Windows[0] as MainWindow).WindowState = WindowState.Minimized;
+        }
+        #endregion
     }
 }
