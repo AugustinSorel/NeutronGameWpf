49e8d36c85b3acb159ab67b8b5f8c16594b5450c
tartie sorel <sorelaugustin@gmail.com>
2021-01-28 18:45:24 +0000

    colors added in the global colors class

---
+++ NeutronGame/Global/GlobalColors.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows.Media;
+
+namespace NeutronGame
+{
+    public static class GlobalColors
+    {
+        #region Const Colors
+
+        private const string LIGHT_GRAY = "#bdc3c7";
+        private const string RED = "#e74c3c";
+
+        private const string TOKEN_PLAYER1 = "#e74c3c";
+        private const string TOKEN_PLAYER2 = "#2ecc71";
+        private const string TOKEN_NEUTRON = "#f39c12";
+
+        #endregion
+
+        #region Tokens Color
+
+        public static SolidColorBrush TokenPlayer1
+        {
+            get { return (SolidColorBrush)new BrushConverter().ConvertFrom(TOKEN_PLAYER1); }
+        }
+
+        public static SolidColorBrush TokenPlayer2
+        {
+            get { return (SolidColorBrush)new BrushConverter().ConvertFrom(TOKEN_PLAYER2); }
+        }
+
+        public static SolidColorBrush TokenNeutron
+        {
+            get { return (SolidColorBrush)new BrushConverter().ConvertFrom(TOKEN_NEUTRON); }
+        }
+
+        #endregion
+
+        public static SolidColorBrush Red
+        {
+            get { return (SolidColorBrush)new BrushConverter().ConvertFrom(RED); }
+        }
+
+        public static SolidColorBrush LightGray
+        {
+            get { return (SolidColorBrush)new BrushConverter().ConvertFrom(LIGHT_GRAY); }
+        }
+
+    }
+}
