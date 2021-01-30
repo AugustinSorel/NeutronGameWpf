85631d05bc0b794a36f39a489fb15833c798bc7d
tartie sorel <sorelaugustin@gmail.com>
2021-01-30 15:43:50 +0000

    Move Token Class

--- NeutronGame/Game/Game.cs
+++ NeutronGame/Game/Game.cs
@@ -76,52 +76,6 @@ namespace NeutronGame
 
         #region Handle Token click Event
 
-        private async void MoveVertically(int goToRow)
-        {
-            while (true)
-            {
-                int currentRow = Grid.GetRow(ellipseSelected);
-                int y = goToRow - currentRow;
-
-                if (y == 0)
-                    break;
-
-                if (y < 0)
-                {
-                    Grid.SetRow(ellipseSelected, currentRow - 1);
-                    await Task.Delay(500);
-                }
-                else if (y > 0)
-                {
-                    Grid.SetRow(ellipseSelected, currentRow + 1);
-                    await Task.Delay(500);
-                }
-            }
-        }
-
-        private async void MoveHorizontally(int goToCol)
-        {
-            while (true)
-            {
-                int currentCol = Grid.GetColumn(ellipseSelected);
-                int x = goToCol - currentCol;
-
-                if (x == 0)
-                    break;
-
-                if (x < 0)
-                {
-                    Grid.SetColumn(ellipseSelected, currentCol - 1);
-                    await Task.Delay(500);
-                }
-                else if (x > 0)
-                {
-                    Grid.SetColumn(ellipseSelected, currentCol + 1);
-                    await Task.Delay(500);
-                }
-            }
-        }
-
         public void HandleTokenClick(object sender)
         {
             if (sender is Button)
@@ -132,24 +86,7 @@ namespace NeutronGame
                 //if (IllegalMove())
                 //    return;
 
-                int goToCol = Grid.GetColumn(sender as Button);
-                int goToRow = Grid.GetRow(sender as Button);
-
-                int currentRow1 = Grid.GetRow(ellipseSelected);
-                int currentCol1 = Grid.GetColumn(ellipseSelected);
-
-                if (Math.Abs(goToRow - currentRow1) > 0 && Math.Abs(goToCol - currentCol1) > 0)
-                {
-                    MoveDiag(goToCol, goToRow);
-                }
-                else if (Math.Abs(goToRow - currentRow1) > 0)
-                {
-                    MoveVertically(goToRow);
-                }
-                else if (Math.Abs(goToCol - currentCol1) > 0)
-                {
-                    MoveHorizontally(goToCol);
-                }
+                new MoveToke(sender, ellipseSelected);
 
                 player1Turn ^= true;
                 SetLabelsColor();
@@ -186,37 +123,6 @@ namespace NeutronGame
             }
 
             return false;
-        }
-
-        private async void MoveDiag(int goToCol, int goToRow)
-        {
-            while (true)
-            {
-                int currentRow = Grid.GetRow(ellipseSelected);
-                int currentCol = Grid.GetColumn(ellipseSelected);
-
-                int y = goToRow - currentRow;
-                int x = goToCol - currentCol;
-
-                if (y == 0 && x == 0)
-                    break;
-
-                if (x < 0)
-                    Grid.SetColumn(ellipseSelected, currentCol - 1);
-                else if (x > 0)
-                    Grid.SetColumn(ellipseSelected, currentCol + 1);
-
-                if (y < 0)
-                {
-                    Grid.SetRow(ellipseSelected, currentRow - 1);
-                    await Task.Delay(500);
-                }
-                else if (y > 0)
-                {
-                    Grid.SetRow(ellipseSelected, currentRow + 1);
-                    await Task.Delay(500);
-                }
-            }
         }
 
         #endregion
---
+++ NeutronGame/Game/MoveToke.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace NeutronGame
+{
+    internal class MoveToke
+    {
+        private readonly Ellipse ellipseSelected;
+
+        public MoveToke(object sender, Ellipse ellipseSelected)
+        {
+            this.ellipseSelected = ellipseSelected;
+
+            int goToCol = Grid.GetColumn(sender as Button);
+            int goToRow = Grid.GetRow(sender as Button);
+
+            int currentRow1 = Grid.GetRow(ellipseSelected);
+            int currentCol1 = Grid.GetColumn(ellipseSelected);
+
+            if (Math.Abs(goToRow - currentRow1) > 0 && Math.Abs(goToCol - currentCol1) > 0)
+            {
+                MoveDiag(goToCol, goToRow);
+            }
+            else if (Math.Abs(goToRow - currentRow1) > 0)
+            {
+                MoveVertically(goToRow);
+            }
+            else if (Math.Abs(goToCol - currentCol1) > 0)
+            {
+                MoveHorizontally(goToCol);
+            }
+        }
+
+        private async void MoveVertically(int goToRow)
+        {
+            while (true)
+            {
+                int currentRow = Grid.GetRow(ellipseSelected);
+                int y = goToRow - currentRow;
+
+                if (y == 0)
+                    break;
+
+                if (y < 0)
+                {
+                    Grid.SetRow(ellipseSelected, currentRow - 1);
+                    await Task.Delay(500);
+                }
+                else if (y > 0)
+                {
+                    Grid.SetRow(ellipseSelected, currentRow + 1);
+                    await Task.Delay(500);
+                }
+            }
+        }
+
+        private async void MoveHorizontally(int goToCol)
+        {
+            while (true)
+            {
+                int currentCol = Grid.GetColumn(ellipseSelected);
+                int x = goToCol - currentCol;
+
+                if (x == 0)
+                    break;
+
+                if (x < 0)
+                {
+                    Grid.SetColumn(ellipseSelected, currentCol - 1);
+                    await Task.Delay(500);
+                }
+                else if (x > 0)
+                {
+                    Grid.SetColumn(ellipseSelected, currentCol + 1);
+                    await Task.Delay(500);
+                }
+            }
+        }
+
+        private async void MoveDiag(int goToCol, int goToRow)
+        {
+            while (true)
+            {
+                int currentRow = Grid.GetRow(ellipseSelected);
+                int currentCol = Grid.GetColumn(ellipseSelected);
+
+                int y = goToRow - currentRow;
+                int x = goToCol - currentCol;
+
+                if (y == 0 && x == 0)
+                    break;
+
+                if (x < 0)
+                    Grid.SetColumn(ellipseSelected, currentCol - 1);
+                else if (x > 0)
+                    Grid.SetColumn(ellipseSelected, currentCol + 1);
+
+                if (y < 0)
+                {
+                    Grid.SetRow(ellipseSelected, currentRow - 1);
+                    await Task.Delay(500);
+                }
+                else if (y > 0)
+                {
+                    Grid.SetRow(ellipseSelected, currentRow + 1);
+                    await Task.Delay(500);
+                }
+            }
+        }
+    }
+}
