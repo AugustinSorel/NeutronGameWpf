13b5f1a7522f6aa18341891f74fa31b6d714967e
tartie sorel <sorelaugustin@gmail.com>
2021-01-31 20:53:11 +0000

    Display choice Diag top left

--- NeutronGame/Game/DisplayAvailableChoices.cs
+++ NeutronGame/Game/DisplayAvailableChoices.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Shapes;
@@ -19,6 +20,10 @@ namespace NeutronGame
 
             DrawUp(startCol, startRow);
             DrawDown(startCol, startRow);
+            DrawLeft(startCol, startRow);
+            DrawRight(startCol, startRow);
+
+            DrawDiagTopLeft(startCol, startRow);
         }
 
         private Button GetAllowedButton(int x, int y)
@@ -41,6 +46,100 @@ namespace NeutronGame
         private bool DistanceBetweenTokenIs1(int x, int y)
         {
             return enumBoard[x, y] != EnumBoard.EmptyCell;
+        }
+
+        private bool BottomWallHited(int startRow, int i)
+        {
+            return startRow + i == 4;
+        }
+
+        private bool TopWallHited(int startRow, int i)
+        {
+            return startRow - i == 0;
+        }
+
+        #endregion
+
+        #region Draw Diag Top Left
+
+        private void DrawDiagTopLeft(int startCol, int startRow)
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                if (startCol - i < 0 || startRow - i < 0)
+                    return;
+
+                if (DistanceBetweenTokenIs1(startCol - 1, startRow -1))
+                    return;
+
+                if (CellNotEmpty(startCol - i, startRow - i))
+                {
+                    GetAllowedButton(startCol - i + 1, startRow - i + 1).Style = GameUserControl.Instance.FindResource("SelectedButton") as Style;
+                    return;
+                }
+
+                if (TopWallHited(startCol, i) || TopWallHited(startRow, i))
+                {
+                    GetAllowedButton(startCol - i, startRow - i).Style = GameUserControl.Instance.FindResource("SelectedButton") as Style;
+                    return;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Draw Left
+
+        private void DrawLeft(int startCol, int startRow)
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                if (startCol - i < 0)
+                    return;
+
+                if (DistanceBetweenTokenIs1(startCol - 1, startRow))
+                    return;
+
+                if (CellNotEmpty(startCol - i, startRow))
+                {
+                    GetAllowedButton(startCol - i + 1, startRow).Style = GameUserControl.Instance.FindResource("SelectedButton") as Style;
+                    return;
+                }
+
+                if (TopWallHited(startCol, i))
+                {
+                    GetAllowedButton(startCol - i, startRow).Style = GameUserControl.Instance.FindResource("SelectedButton") as Style;
+                    return;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Draw Right
+
+        private void DrawRight(int startCol, int startRow)
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                if (startCol + i > 4)
+                    return;
+
+                if (DistanceBetweenTokenIs1(startCol + 1, startRow))
+                    return;
+
+                if (CellNotEmpty(startCol + i, startRow))
+                {
+                    GetAllowedButton(startCol + i - 1, startRow).Style = GameUserControl.Instance.FindResource("SelectedButton") as Style;
+                    return;
+                }
+
+                if (BottomWallHited(startCol, i))
+                {
+                    GetAllowedButton(startCol + i, startRow).Style = GameUserControl.Instance.FindResource("SelectedButton") as Style;
+                    return;
+                }
+            }
         }
 
         #endregion
@@ -69,11 +168,6 @@ namespace NeutronGame
                 }
             }
         }
-
-        private bool BottomWallHited(int startRow, int i)
-        {
-            return startRow + i == 4;
-        }
         #endregion
 
         #region Draw Up
@@ -100,12 +194,6 @@ namespace NeutronGame
                 }
             }
         }
-
-        private static bool TopWallHited(int startRow, int i)
-        {
-            return startRow - i == 0;
-        }
-
         #endregion 
     }
 }
